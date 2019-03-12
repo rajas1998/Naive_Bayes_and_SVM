61afea7f1bdd0505de10809c407d7d5b5256dc72
Rajas Bansal <rajas@Rajass-MacBook-Pro.local>
2019-03-12 17:06:29 +0530

    first commit

---
+++ nb.py
@@ -0,0 +1,132 @@
+import pandas as pd
+import numpy as np
+import nltk
+from nltk.tokenize import word_tokenize
+# from ass2_data import utils
+import string
+import time
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
+import math
+import random
+from sklearn.metrics import confusion_matrix
+from nltk.stem.porter import PorterStemmer
+from sklearn.metrics import classification_report
+
+p_stem = PorterStemmer()
+stopwords = False
+stemming = False
+
+start = time.time()
+training_data = pd.read_json("ass2_data/train.json", lines = True)
+end = time.time()
+print ("Finished loading the train data ", (end - start))
+
+x = training_data['text']
+y = training_data['stars']
+training_data = None
+m = len(y)
+
+if (stopwords):
+	analyzer = CountVectorizer(stop_words = "english").build_analyzer()
+else:
+	analyzer = CountVectorizer(ngram_range=(1,1)).build_analyzer()
+vocab = {}
+start = time.time()
+x = [analyzer(i) for i in x]
+def stem(inp):
+	return [p_stem.stem(i) for i in inp]
+if (stemming):
+	x = [stem(i) for i in x]
+end = time.time()
+print ("Finished the tokenization ",end - start)
+
+start = time.time()
+count = 0
+for i in x:
+	for j in i:
+		if j not in vocab:
+			vocab[j] = count
+			count += 1
+end = time.time()
+print (end - start)
+
+param = [[1.0 for i in range(count)] for j in range(5)]
+
+start = time.time()
+for i in range(len(x)):
+	for j in x[i]:
+		param[y[i] - 1][vocab[j]] += 1
+	if (i % 50000 == 0):
+		print (i)
+end = time.time()
+print (end - start)
+
+start = time.time()
+sum_param = [sum(i) for i in param]
+for i in range(len(param)):
+	for j in range(len(param[i])):
+		param[i][j] = math.log(param[i][j]/sum_param[i])
+end = time.time()
+print (end - start)
+
+y_param = [0.0 for i in range(5)]
+for i in y:
+	y_param[i-1] += 1
+y_param = [math.log(i/m) for i in y_param]
+print (y_param)
+
+def predict(inp):
+	s = [y_param[i] for i in range(5)]
+	for i in inp:
+		if i in vocab:
+			for j in range(5):
+				s[j] += param[j][vocab[i]]
+	return (s.index(max(s)) + 1)
+
+def accuracy(inp,y,m):
+	cnt = 0
+	for i in range(m):
+		if (inp[i] == y[i]):
+			cnt += 1
+	return ((cnt/m)*100)
+start = time.time()	
+pred_y = [predict(i) for i in x]
+end = time.time()
+print (end - start)
+
+cnt = 0
+for i in range(m):
+	if (pred_y[i] == y[i]):
+		cnt += 1
+print ("Accuracy over training data = ", (cnt/m) * 100)
+
+start = time.time()
+test_data = pd.read_json("ass2_data/test.json", lines = True)
+end = time.time()
+print ("Finished loading the test data ", (end - start))
+
+x = test_data['text']
+y = test_data['stars']
+test_data = None
+m = len(y)
+x = [analyzer(i) for i in x]
+print ("Finished the tokenization")
+pred_y = [predict(i) for i in x]
+cnt = 0
+for i in range(m):
+	if (pred_y[i] == y[i]):
+		cnt += 1
+print ("Accuracy over test data = ", (cnt/m) * 100)
+print ("Classification Report: ")
+print (classification_report(y,pred_y))
+
+pred_y = [random.randint(1,5) for i in range(m)]
+print ("Accuracy with random guess = ", (accuracy(pred_y,y,m)))
+
+elem = y_param.index(max(y_param)) + 1
+pred_y = [elem for i in range(m)]
+print ("Accuracy with majority guess = ", (accuracy(pred_y,y,m)))
+
+pred_y = [predict(i) for i in x]
+print ("Confusion Matrix")
+print (confusion_matrix(y,pred_y))
---
+++ svm_all.py
@@ -0,0 +1,259 @@
+import numpy as np
+from cvxopt import matrix, solvers
+import math
+import time
+from svmutil import *
+from scipy.spatial.distance import pdist, squareform
+import scipy
+from sklearn.model_selection import train_test_split
+
+def read_csv(filename):
+	f = open(filename,"r")
+	x = f.readlines()
+	f.close()
+	x = [list(map(float, i.split(","))) for i in x]
+	y = [int(i[-1]) for i in x]
+	x = [np.array(i[:-1])/255 for i in x]
+	y = np.array(y)
+	x = np.array(x)
+	return (x,y)
+
+def dual_class(x,y,a,b):
+	x_reduced = []
+	y_reduced = []
+	m = len(y)
+	for i in range(m):
+		if (y[i] == a):
+			x_reduced.append(x[i])
+			y_reduced.append(-1)
+		elif (y[i] == b):
+			x_reduced.append(x[i])
+			y_reduced.append(1)
+	return (np.array(x_reduced), np.array(y_reduced))
+
+def fit_linear(x_in, y_in, c): 
+	m_in = len(y_in)
+	print (x_in.shape)
+	K = y_in[:,None] * x_in
+	K = np.dot(K, K.T)
+	P = matrix(K)
+	q = matrix(-np.ones((m_in, 1)))
+	G = matrix(np.concatenate((-np.eye(m_in),np.eye(m_in))))
+	h = matrix(np.concatenate((np.zeros(m_in),c*np.ones(m_in))))
+	A = (y_in.reshape(1, -1))
+	A = A.astype(float)
+	A = matrix(A)
+	b = matrix(np.zeros(1))
+	solvers.options['show_progress'] = True
+	sol = solvers.qp(P, q, G, h, A, b)
+	alphas = np.array(sol['x'])
+	w = np.sum(alphas * y_in[:, None] * x_in, axis = 0)
+	bias = 0
+	for i in range(len(alphas)):
+		if (alphas[i] > 1e-4 and alphas[i] < c - 1e-4):
+			bias = y_in[i] - np.dot(x_in[i], w)
+			break
+	return (alphas,w,bias)
+
+def gaussian_multiply(x_in,y_in,alphas,non_zeros,gamma,x):
+	ret = 0.0
+	for i in non_zeros:
+		ret += alphas[i]*y_in[i]*math.exp((-1)*gamma*(np.linalg.norm(x_in[i]-x)**2))
+	return ret
+def fit_gaussian(x_in, y_in, c, gamma): 
+	m_in = len(y_in)
+	print (x_in.shape)
+	start = time.time()
+	K = squareform(pdist(x_in, 'euclidean'))
+	K = scipy.exp((-1) * (K ** 2) * gamma)
+	yv = y_in.reshape(-1, 1)
+	ydash = np.matmul(yv,np.transpose(yv))
+	K = K * ydash
+	print (ydash.shape)
+	print (K.shape)
+	print (K)
+	# K = np.zeros((m_in,m_in))
+	# for i in range(m_in):
+	# 	# print i
+	# 	for j in range(m_in):
+	# 		K[i][j] = math.exp((-1)*gamma*(np.linalg.norm(x_in[i]-x_in[j])**2))*y_in[i]*y_in[j]
+	# print (K)
+	# print (K.shape)
+	end = time.time()
+	print "The time spent here is",end-start
+	P = matrix(K)
+	q = matrix(-np.ones((m_in, 1)))
+	G = matrix(np.concatenate((-np.eye(m_in),np.eye(m_in))))
+	h = matrix(np.concatenate((np.zeros(m_in),c*np.ones(m_in))))
+	A = (y_in.reshape(1, -1))
+	A = A.astype(float)
+	A = matrix(A)
+	b = matrix(np.zeros(1))
+	solvers.options['show_progress'] = True
+	sol = solvers.qp(P, q, G, h, A, b)
+	alphas = np.array(sol['x'])
+	non_zeros = []
+	for i in range(len(alphas)):
+		if (alphas[i] > 1e-4):
+			non_zeros.append(i)
+	for i in range(len(alphas)):
+		if (alphas[i] > 1e-4 and alphas[i] < c - 1e-4):
+			bias = y_in[i] - gaussian_multiply(x_in,y_in,alphas,non_zeros,gamma,x_in[i])
+			break
+	return (alphas,non_zeros,bias)
+
+def test_accuracy_linear(x,y,w,b):
+	pred_y = []
+	for i in range(len(x)):
+		if (np.dot(x[i], w) + b >= 0):
+			pred_y.append(1)
+		else :
+			pred_y.append(-1)
+	cnt = 0
+	for i in range(len(y)):
+		if (y[i] == pred_y[i]):
+			cnt += 1
+	return (float(cnt)/len(y))
+
+def test_accuracy_gaussian(x,y,b,alphas,non_zeros,gamma,x_in,y_in):
+	pred_y = []
+	for i in range(len(x_in)):
+		if (gaussian_multiply(x,y,alphas,non_zeros,gamma,x_in[i]) + b >= 0):
+			pred_y.append(1)
+		else :
+			pred_y.append(-1)
+	cnt = 0
+	for i in range(len(y_in)):
+		if (y_in[i] == pred_y[i]):
+			cnt += 1
+	return (float(cnt)/len(y_in))
+
+x_train, y_train = read_csv("mnist/train.csv")
+x_test, y_test = read_csv("mnist/test.csv")
+
+# First part
+# x_a, y_a = dual_class(x_train, y_train, 5, 6)
+# start = time.time()
+# alpha, w, b = fit_linear(x_a, y_a, 1.0)
+# end = time.time()
+# print "Time taken for linear is ", (end - start)
+# x_test_a, y_test_a = dual_class(x_test, y_test, 5, 6)
+# num_sup = 0
+# print "The support vectors are:"
+# for i in range(len(y_a)):
+# 	if ((alpha[i] > 1e-4)):
+# 		# print i,
+# 		num_sup += 1
+# print
+# print "The number of support vectors are ",num_sup
+# print "The training set accuracy is", test_accuracy_linear(x_a,y_a,w,b)
+# print "The test set accuracy is", test_accuracy_linear(x_test_a, y_test_a, w, b)
+
+#Second Part 
+# gamma = 0.05
+# x_a, y_a = dual_class(x_train, y_train, 5, 6)
+# start = time.time()
+# alphas, non_zeros, b = fit_gaussian(x_a, y_a, 1.0, gamma)
+# end = time.time()
+# print "Time taken for gaussian is ", (end - start)
+# x_test_a, y_test_a = dual_class(x_test, y_test, 5, 6)
+# num_sup = 0
+# print "The support vectors are:"
+# for i in range(len(y_a)):
+# 	if ((alphas[i] > 1e-4)):
+# 		# print i,
+# 		num_sup += 1
+# print
+# print "The number of support vectors are ",num_sup
+# print "The training set accuracy is", test_accuracy_gaussian(x_a,y_a,b,alphas,non_zeros,gamma, x_a, y_a)
+# print "The test set accuracy is", test_accuracy_gaussian(x_a, y_a, b, alphas, non_zeros, gamma, x_test_a, y_test_a)
+
+#Third part
+# x_a, y_a = dual_class(x_train, y_train, 5, 6)
+# x_test_a, y_test_a = dual_class(x_test, y_test, 5, 6)
+# prob  = svm_problem(y_a, x_a)
+# param = svm_parameter('-t 0 -c 1')
+# m = svm_train(prob, param)
+# print "Training set: "
+# p_label, p_acc, p_val = svm_predict(y_a, x_a, m)
+# # print (p_label,p_acc,p_val)
+# print "Test set: "
+# p_label, p_acc, p_val = svm_predict(y_test_a, x_test_a, m)
+# # print (p_label,p_acc,p_val)
+
+#Fourth part
+# x_a, y_a = dual_class(x_train, y_train, 5, 6)
+# x_test_a, y_test_a = dual_class(x_test, y_test, 5, 6)
+# prob  = svm_problem(y_a, x_a)
+# param = svm_parameter('-t 2 -c 1 -g 0.05')
+# m = svm_train(prob, param)
+# print "Training set: "
+# p_label, p_acc, p_val = svm_predict(y_a, x_a, m)
+# # print (p_label,p_acc,p_val)
+# print "Test set: "
+# p_label, p_acc, p_val = svm_predict(y_test_a, x_test_a, m)
+
+#Q2a
+# models = []
+# for i in range(10):
+# 	for j in range(i+1,10):
+# 		print i, ",",j,":- "
+# 		x_a, y_a = dual_class(x_train, y_train, i, j)
+# 		prob  = svm_problem(y_a, x_a)
+# 		param = svm_parameter('-t 2 -c 1 -g 0.05')
+# 		m = svm_train(prob, param)
+# 		models.append((m,i,j))	
+# labels = []
+# for m in models:
+# 	p_label, p_acc, p_val = svm_predict(y_train,x_train, m[0])
+# 	labels.append((p_label,m[1],m[2]))
+# pred_y = []
+# for i in range(len(x_train)):
+# 	w = [0 for j in range(10)]
+# 	for x in labels:
+# 		if (x[0][i] == 1):
+# 			w[x[2]] += 1
+# 		else:
+# 			w[x[1]] += 1
+# 	pred_y.append(w.index(max(w)))
+# cnt = 0
+# for i in range(len(x_train)):
+# 	if (y_train[i] == pred_y[i]):
+# 		cnt += 1
+# print "The accuracy over the training set is", float(cnt)/len(x_train)
+# labels = []
+# for m in models:
+# 	p_label, p_acc, p_val = svm_predict(y_test,x_test, m[0])
+# 	labels.append((p_label,m[1],m[2]))
+# pred_y = []
+# for i in range(len(x_test)):
+# 	w = [0 for j in range(10)]
+# 	for x in labels:
+# 		if (x[0][i] == 1):
+# 			w[x[2]] += 1
+# 		else:
+# 			w[x[1]] += 1
+# 	pred_y.append(w.index(max(w)))
+# cnt = 0
+# for i in range(len(x_test)):
+# 	if (y_test[i] == pred_y[i]):
+# 		cnt += 1
+# print "The accuracy over the test set is", float(cnt)/len(x_test)
+
+#Q2b
+trainingSet_x, validationSet_x, trainingSet_y, validationSet_y = train_test_split(x_train, y_train, test_size=0.1)
+start = time.time()
+prob  = svm_problem(trainingSet_y, trainingSet_x)
+for c in [10**(-5),10**(-3),1,5,10]:
+	print "Checking for c = "+str(c)
+	param = svm_parameter('-t 2 -c '+str(c)+' -g 0.05')
+	m = svm_train(prob, param)
+	print "Validation set: "
+	p_label, p_acc, p_val = svm_predict(validationSet_y, validationSet_x, m)
+	print "Test set: "
+	p_label, p_acc, p_val = svm_predict(y_test, x_test, m)
+# print "Confusion Matrix:"
+end = time.time()
+print (end - start)
+print (confusion_matrix(y_test,p_label))
+
